d5a06574630e514ed27ca0f4761972abb5ed6115
尐 <313303303@qq.com>
2018-06-07 18:55:03 +0800

    Update __init__.py

--- cprint/__init__.py
+++ cprint/__init__.py
@@ -4,8 +4,7 @@ from .cprint import cprint
 from .Cprint import Cprint
 
 
-_cprint = cprint
-cprint = Cprint().cprint()
+ccprint = Cprint().cprint()
 
 """
     This module give to possibility to print in color.
